540f845c3ab4cde701375606ff3e7b68f8ce0326
Pat Hickey <pat@moreproductive.org>
2021-03-03 12:27:28 -0800

    test: add a trivial runner

--- crates/wiggle/tests/atoms_async.rs
+++ crates/wiggle/tests/atoms_async.rs
@@ -1,4 +1,7 @@
 use proptest::prelude::*;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll, RawWaker, RawWakerVTable, Waker};
 use wiggle::GuestMemory;
 use wiggle_test::{impl_errno, HostMemory, MemArea, WasiCtx};
 
@@ -12,7 +15,7 @@ wiggle::from_witx!({
 
 impl_errno!(types::Errno, types::GuestErrorConversion);
 
-#[wiggle::async_trait]
+#[wiggle::async_trait(?Send)]
 impl<'a> atoms::Atoms for WasiCtx<'a> {
     async fn int_float_args(&self, an_int: u32, an_float: f32) -> Result<(), types::Errno> {
         println!("INT FLOAT ARGS: {} {}", an_int, an_float);
@@ -39,7 +42,12 @@ impl IntFloatExercise {
         let ctx = WasiCtx::new();
         let host_memory = HostMemory::new();
 
-        let e = atoms::int_float_args(&ctx, &host_memory, self.an_int as i32, self.an_float);
+        let e = run(atoms::int_float_args(
+            &ctx,
+            &host_memory,
+            self.an_int as i32,
+            self.an_float,
+        ));
 
         assert_eq!(e, Ok(types::Errno::Ok as i32), "int_float_args error");
     }
@@ -68,12 +76,12 @@ impl DoubleIntExercise {
         let ctx = WasiCtx::new();
         let host_memory = HostMemory::new();
 
-        let e = atoms::double_int_return_float(
+        let e = run(atoms::double_int_return_float(
             &ctx,
             &host_memory,
             self.input as i32,
             self.return_loc.ptr as i32,
-        );
+        ));
 
         let return_val = host_memory
             .ptr::<types::AliasToFloat>(self.return_loc.ptr)
@@ -94,5 +102,39 @@ proptest! {
     #[test]
     fn double_int_return_float(e in DoubleIntExercise::strat()) {
         e.test()
+    }
+}
+
+fn run<F: Future>(future: F) -> F::Output {
+    let mut f = Pin::from(Box::new(future));
+    let waker = dummy_waker();
+    let mut cx = Context::from_waker(&waker);
+    loop {
+        match f.as_mut().poll(&mut cx) {
+            Poll::Ready(val) => break val,
+            Poll::Pending => {}
+        }
+    }
+}
+
+fn dummy_waker() -> Waker {
+    return unsafe { Waker::from_raw(clone(5 as *const _)) };
+
+    unsafe fn clone(ptr: *const ()) -> RawWaker {
+        assert_eq!(ptr as usize, 5);
+        const VTABLE: RawWakerVTable = RawWakerVTable::new(clone, wake, wake_by_ref, drop);
+        RawWaker::new(ptr, &VTABLE)
+    }
+
+    unsafe fn wake(ptr: *const ()) {
+        assert_eq!(ptr as usize, 5);
+    }
+
+    unsafe fn wake_by_ref(ptr: *const ()) {
+        assert_eq!(ptr as usize, 5);
+    }
+
+    unsafe fn drop(ptr: *const ()) {
+        assert_eq!(ptr as usize, 5);
     }
 }
